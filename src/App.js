8b58bff62640db49445b8f8ae94e21d3447f455e
Osama Yousuf <osamayousuf26@gmail.com>
2024-01-02 13:32:55 +0500

    'fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen bg-black w-full flex flex-col justify-center items-center'>
+      <div className='h-96 w-460 bg-white rounded-[20px] '>
+        <p className="text-center p-t">Welcome User</p>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      width: {
+        150: "150px",
+        190: "190px",
+        225: "225px",
+        275: "275px",
+        300: "300px",
+        340: "340px",
+        350: "350px",
+        375: "375px",
+        460: "460px",
+        656: "656px",
+        880: "880px",
+        508: "508px",
+      },
+      height: {
+        80: "80px",
+        150: "150px",
+        225: "225px",
+        300: "300px",
+        340: "340px",
+        370: "370px",
+        420: "420px",
+        510: "510px",
+        600: "600px",
+        650: "650px",
+        685: "685px",
+        800: "800px",
+        "90vh": "90vh",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        primary: "#768297",
+        secondary: "#0E2E50",
+        blackbg: "#212120",
+        input: "#525589",
+        buttonbg: "#0075FF",
+        header: "#080C2E"
+      },
+    },
+  },
+  plugins: [
+
+  ],
+}
